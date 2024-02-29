2bd1c8f1a8d8ca39d5ce1b3764e99c4ab7c44199
yasai-nodame <135739623+yasai-nodame@users.noreply.github.com>
2024-02-29 15:41:23 +0900

    note自動投稿ツール

--- note.py
+++ note.py
@@ -1,6 +1,5 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
